ae130e29a5c818060d4a21ff23039ecffad9bbc9
Rodriguez <55442966+SergioRB123@users.noreply.github.com>
2020-02-12 08:23:03 -0500

    cambios hechos por sergio

--- funciones.js
+++ funciones.js
@@ -8,4 +8,10 @@ window.alert("hola")
 window.alert("hola")
 window.alert("hola")
 window.alert("hola")
+window.alert("hola")
+window.alert("hola")
+window.alert("hola")
+window.alert("hola")
+window.alert("hola")
+window.alert("hola");
 
